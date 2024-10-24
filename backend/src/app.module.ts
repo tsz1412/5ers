dc37c4912fadad9205127b7161e9340a7435c82e
Tsviel Zaikman <tsvielzaikman@Tsviels-MacBook-Pro.local>
2024-10-24 15:49:54 +0300

    initial commit. initial docker setup. nestjs setup. Users Logic. Auth Logic(passport)

---
+++ backend/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.MONGO_URI), // Replace with your MongoDB connection string
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+
+
+export class AppModule {}
