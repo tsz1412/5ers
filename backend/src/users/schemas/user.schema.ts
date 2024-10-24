dd49f93a0fca914d9253b03c579bca381de625a2
Tsviel Zaikman <tsvielzaikman@Tsviels-MacBook-Pro.local>
2024-10-25 01:02:54 +0300

    prevent add the same stock twice on backend

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -10,25 +10,40 @@ export class User extends Document {
     @Prop({ required: true })
     password: string;
 
-    @Prop({ type: [String], default: [] })
+    @Prop({
+        type: [String],
+        default: [],
+        validate: {
+            validator: function(value: string[]) {
+                // Check for duplicate entries in the array
+                return Array.isArray(value) && new Set(value).size === value.length;
+            },
+            message: 'Each stock symbol must be unique',
+        },
+    })
     stocks: string[]; // Stock symbols that user holds
 
     comparePassword: (candidatePassword: string) => Promise<boolean>;
-
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
 // Middleware for hashing password before saving
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 // Add the comparePassword method to the schema's methods object
-UserSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+UserSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
     return bcrypt.compare(candidatePassword, this.password);
 };
+
+// Pre-save hook to ensure uniqueness of stocks before saving
+UserSchema.pre('save', function (next) {
+    this.stocks = [...new Set(this.stocks)]; // Remove duplicates before saving
+    next();
+});
